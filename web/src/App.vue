fec88d5613c60ab5ea73d94489072f2579730b21
Emmanuel Lodovice <name3anad@gmail.com>
2018-06-12 01:42:17 +0800

    Added navbar component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <p>Youtube DL</p>
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
+import Navbar from './navbar.vue';
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  components: {
+    navbar: Navbar
   }
 }
 </script>
 
 <style lang="scss">
+body {
+  margin: 0px;
+  background-color: #1c1c1c;
+}
+
+@font-face {
+  font-family: "NotoSans";
+  src: url("assets/NotoSans-Bold.ttf");
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: "NotoSansCondensed";
+  src: url("assets/NotoSans-ExtraCondensedBold.ttf");
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
