ebc19ebf61cf3866acaf1a30eaafb7f56c4d7856
Emmanuel Lodovice <name3anad@gmail.com>
2018-06-13 01:38:31 +0800

    Added new component containing the download form

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="app">
-    <navbar></navbar>
-  </div>
+    <div id="app">
+        <navbar></navbar>
+        <download-form></download-form>
+    </div>
 </template>
 
 <script>
 import Navbar from './navbar.vue';
+import DownloadForm from './download_form.vue';
 
 export default {
   name: 'app',
   components: {
-    navbar: Navbar
+    navbar: Navbar,
+    downloadForm: DownloadForm
   }
 }
 </script>
 
 <style lang="scss">
+html {
+    height: 100%;
+}
 body {
   margin: 0px;
-  background-color: #1c1c1c;
+  background-color: #121212;
+  height: 100%;
 }
 
 @font-face {
   font-family: "NotoSans";
   src: url("assets/NotoSans-Bold.ttf");
   font-weight: bold;
+}
+
+@font-face {
+  font-family: "NotoSans";
+  src: url("assets/NotoSans-Regular.ttf");
 }
 
 @font-face {
@@ -38,6 +50,9 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+
+  font-family: "NotoSans";
 }
 
 h1, h2 {
---
+++ web/src/download_form.vue
@@ -0,0 +1,85 @@
+<template>
+    <aside id="main">
+        <div class="container">
+        <h1>DOWNLOAD VIDEOS FROM YOUTUBE</h1>
+        <form>
+            <div class="formset">
+                <label for="url">PASTE LINK HERE</label>
+                <input type="text" id="url" name="url" placeholder="e.g. https://www.youtube.com/watch?v=vmGlQSACCIA">
+            </div>
+            <button>DOWNLOAD</button>
+        </form>
+        </div>
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'downloadForm'
+}
+</script>
+
+<style lang="scss" scoped>
+
+#main {
+    height: 80%;
+    width: 100%;
+    display: flex;
+}
+
+.container {
+    margin: auto;
+}
+
+h1 {
+    color: #ffffff;
+    font-family: "NotoSans";
+    font-weight: bold;
+    font-size: 22px;
+}
+
+.formset {
+    background-color: #232323;
+    padding: 15px 20px;
+    border-radius: 3px;
+    border: 0.5px solid #4c4c4c;
+    text-align: left;
+    vertical-align: middle;
+    margin-top: 40px;
+
+    label {
+        color: #6d6d6d;
+        font-family: "NotoSans";
+        text-align: left;
+        font-size: 14px;
+        line-height: 23px;
+        vertical-align: top;
+        font-weight: bold;
+    }
+
+    input {
+        background-color: #232323;
+        border: none;
+        color: #6d6d6d;
+        width: 300px;
+        margin-left: 20px;
+        font-style: italic;
+        font-size: 12px;
+        padding: 5px;
+    }
+}
+
+button {
+    margin-top: 40px;
+    background-color: #ff0000;
+    border: none;
+    border-radius: 3px;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 17px;
+    padding: 15px 30px;
+    font-family: "NotoSans";
+
+}
+
+</style>
